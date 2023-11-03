a48418df1907ad0cc7343c661f0d59f27c24ecc7
Olufemi Okikioluwa <kikkyboysven@gmail.com>
2023-11-03 13:09:46 +0100

    chore: format code:

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,57 +10,57 @@ import type { ImageProps } from "../utils/types";
 import { useLastViewedPhoto } from "../utils/useLastViewedPhoto";
 
 const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
-	const router = useRouter();
-	const { photoId } = router.query;
-	const [lastViewedPhoto, setLastViewedPhoto] = useLastViewedPhoto();
+  const router = useRouter();
+  const { photoId } = router.query;
+  const [lastViewedPhoto, setLastViewedPhoto] = useLastViewedPhoto();
 
-	const lastViewedPhotoRef = useRef<HTMLAnchorElement>(null);
+  const lastViewedPhotoRef = useRef<HTMLAnchorElement>(null);
 
-	useEffect(() => {
-		// This effect keeps track of the last viewed photo in the modal to keep the index page in sync when the user navigates back
-		if (lastViewedPhoto && !photoId) {
-			lastViewedPhotoRef.current.scrollIntoView({ block: "center" });
-			setLastViewedPhoto(null);
-		}
-	}, [photoId, lastViewedPhoto, setLastViewedPhoto]);
+  useEffect(() => {
+    // This effect keeps track of the last viewed photo in the modal to keep the index page in sync when the user navigates back
+    if (lastViewedPhoto && !photoId) {
+      lastViewedPhotoRef.current.scrollIntoView({ block: "center" });
+      setLastViewedPhoto(null);
+    }
+  }, [photoId, lastViewedPhoto, setLastViewedPhoto]);
 
-	return (
-		<>
-			<Head>
-				<title>Sout West Economic Summit</title>
-				<meta
-					property='og:image'
-					content='https://nextjsconf-pics.vercel.app/og-image.png'
-				/>
-				<meta
-					name='twitter:image'
-					content='https://nextjsconf-pics.vercel.app/og-image.png'
-				/>
-			</Head>
-			<main className='mx-auto h-screen max-w-[1960px] p-4'>
-				{photoId && (
-					<Modal
-						images={images}
-						onClose={() => {
-							setLastViewedPhoto(photoId);
-						}}
-					/>
-				)}
-				<div className='after:content relative mb-5 flex h-full flex-col items-center justify-end gap-2 overflow-hidden rounded-lg bg-white/10 px-6 py-16 text-center text-white shadow-highlight after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight lg:pt-0'>
-					<div className='absolute inset-0 flex items-center justify-center opacity-20'>
-						<span className='flex max-h-full max-w-full items-center justify-center'>
-							<Bridge />
-						</span>
-						<span className='absolute bottom-0 left-0 right-0 h-[400px] bg-gradient-to-b from-black/0 via-black to-black'></span>
-					</div>
-					<h1 className='mb-4 mt-8 text-base font-bold uppercase tracking-widest'>
-						Sout West Economic Summit
-					</h1>
-					<p className='max-w-[40ch] text-white/75 sm:max-w-[32ch]'>
-						Anticipate...
-					</p>
-				</div>
-				{/* <div className='hidden columns-1 gap-4 sm:columns-2 xl:columns-3 2xl:columns-4'>
+  return (
+    <>
+      <Head>
+        <title>Sout West Economic Summit</title>
+        <meta
+          property="og:image"
+          content="https://nextjsconf-pics.vercel.app/og-image.png"
+        />
+        <meta
+          name="twitter:image"
+          content="https://nextjsconf-pics.vercel.app/og-image.png"
+        />
+      </Head>
+      <main className="mx-auto h-screen max-w-[1960px] p-4">
+        {photoId && (
+          <Modal
+            images={images}
+            onClose={() => {
+              setLastViewedPhoto(photoId);
+            }}
+          />
+        )}
+        <div className="after:content relative mb-5 flex h-full flex-col items-center justify-end gap-2 overflow-hidden rounded-lg bg-white/10 px-6 py-16 text-center text-white shadow-highlight after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight lg:pt-0">
+          <div className="absolute inset-0 flex items-center justify-center opacity-20">
+            <span className="flex max-h-full max-w-full items-center justify-center">
+              <Bridge />
+            </span>
+            <span className="absolute bottom-0 left-0 right-0 h-[400px] bg-gradient-to-b from-black/0 via-black to-black"></span>
+          </div>
+          <h1 className="mb-4 mt-8 text-base font-bold uppercase tracking-widest">
+            Sout West Economic Summit
+          </h1>
+          <p className="max-w-[40ch] text-white/75 sm:max-w-[32ch]">
+            Anticipate...
+          </p>
+        </div>
+        {/* <div className='hidden columns-1 gap-4 sm:columns-2 xl:columns-3 2xl:columns-4'>
 					{[].map(({ id, public_id, format, blurDataUrl }) => (
 						<Link
 							key={id}
@@ -87,9 +87,9 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
 						</Link>
 					))}
 				</div> */}
-			</main>
-		</>
-	);
+      </main>
+    </>
+  );
 };
 
 export default Home;
