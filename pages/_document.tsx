6be8142f0ad939e39bf500ec354a01d411fc26d8
Olufemi Okikioluwa <kikkyboysven@gmail.com>
2023-11-05 19:50:43 +0100

    chore: update site metadata

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,26 +1,26 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          <link rel="icon" href="/favicon.ico" />
-          <meta name="description" content="" />
-          <meta property="og:site_name" content="Sout West Economic Summit" />
-          <meta property="og:description" content="" />
-          <meta property="og:title" content="Sout West Economic Summit" />
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content="Sout West Economic Summit" />
-          <meta name="twitter:description" content="" />
-        </Head>
-        <body className="bg-black antialiased">
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+	render() {
+		return (
+			<Html lang='en'>
+				<Head>
+					<link rel='icon' href='/favicon.ico' />
+					<meta name='description' content='' />
+					<meta property='og:site_name' content='South West Economic Summit' />
+					<meta property='og:description' content='' />
+					<meta property='og:title' content='South West Economic Summit' />
+					<meta name='twitter:card' content='summary_large_image' />
+					<meta name='twitter:title' content='South West Economic Summit' />
+					<meta name='twitter:description' content='' />
+				</Head>
+				<body className='bg-black antialiased'>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
 }
 
 export default MyDocument;
